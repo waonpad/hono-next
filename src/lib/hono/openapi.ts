5da03e1ec69c9eb548a3f4e4561b403bb040e434
waonpad <waonpad@gmail.com>
2024-09-23 03:27:27 +0900

    あうあう

--- src/lib/hono/openapi.ts
+++ src/lib/hono/openapi.ts
@@ -1,34 +1,26 @@
 import { swaggerUI } from "@hono/swagger-ui";
 import type { OpenAPIHono } from "@hono/zod-openapi";
 
+const SPEC_PATH = "/spec" as const;
+
 /**
  * Open APIドキュメント関連の設定
  */
 export const docs = (app: OpenAPIHono) => {
   app
     /**
-     * openapiドキュメントそのもの
+     * ドキュメントそのもの
      */
-    .doc31("/specification", (c) => ({
+    .doc31(SPEC_PATH, (c) => ({
       openapi: "3.1.0",
       info: {
         title: "API",
         version: "1.0.0",
       },
-      servers: [
-        {
-          url: new URL(c.req.url).origin,
-          description: "Current environment",
-        },
-      ],
+      servers: [{ url: new URL(c.req.url).origin }],
     }))
     /**
-     * swagger-ui \
+     * Swagger UI
      */
-    .get(
-      "/doc",
-      swaggerUI({
-        url: "/api/specification",
-      }),
-    );
+    .get("/doc", swaggerUI({ url: `/api${SPEC_PATH}` }));
 };
