ec8ff37752f7d6c4e4ccbdaf57b33c795a8b90e1
waonpad <waonpad@gmail.com>
2024-09-22 14:03:49 +0900

    にょ

--- src/lib/hono/client.ts
+++ src/lib/hono/client.ts
@@ -1,6 +1,6 @@
 import { hc } from "hono/client";
 
 import type { AppType } from "@/app/api/[[...route]]/route";
-import { clientEnv } from "@/config/env";
+import { clientEnv } from "@/config/env/client";
 
 export const client = hc<AppType>(clientEnv.NEXT_PUBLIC_HOST_URL);
